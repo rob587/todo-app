91a5fc992dc058720b52a9cd4cc4324a18f1cf6d
rob587 <robertocamm02@yahoo.com>
2026-04-10 15:39:52 +0200

    added datas and add-task function

--- src/app/services/todo.ts
+++ src/app/services/todo.ts
@@ -1,6 +1,57 @@
 import { Injectable } from '@angular/core';
 
+export interface Todo {
+  id: number;
+  nome: string;
+  status: boolean;
+}
+
 @Injectable({
   providedIn: 'root',
 })
-export class Todo {}
+export class TodoService {
+  grandeTitolo = 'La tua lista personale';
+
+  private todos: Todo[] = [
+    {
+      id: 1,
+      nome: 'cico',
+      status: false,
+    },
+    {
+      id: 2,
+      nome: 'popo',
+      status: true,
+    },
+    {
+      id: 3,
+      nome: 'dodo',
+      status: false,
+    },
+    {
+      id: 4,
+      nome: 'pupu',
+      status: true,
+    },
+    {
+      id: 5,
+      nome: 'dende',
+      status: false,
+    },
+  ];
+
+  getTodos() {
+    return this.todos;
+  }
+
+  taskAggiunta = false;
+
+  taskNuova = '';
+  nuovoStatus = '';
+
+  aggiungiTodo(nome: string) {
+    if (this.taskNuova.trim() === '') {
+      this.taskAggiunta = true;
+    }
+  }
+}
